1850c84ece874fd19dd4e9e01cd4d68e4867a0eb
CebrailDevOps <noobione-noobiwan@outlook.com>
2023-08-25 10:12:57 +0200

    edit init.sql nfs.yml sprin.yml

--- init.sql
+++ init.sql
@@ -1,14 +1,14 @@
 -- init.sql
 USE mysonet;
 
-CREATE TABLE mysonetusers (
+CREATE TABLE IF NOT EXISTS mysonetusers (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     ip_add VARCHAR(40) NOT NULL UNIQUE,
     creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE demandes_ami (
+CREATE TABLE IF NOT EXISTS demandes_ami (
     id INT AUTO_INCREMENT PRIMARY KEY,
     id_demandeur INT NOT NULL,
     id_demande INT NOT NULL,
